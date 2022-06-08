88459becf4af8c31f7a5e1894874542872543e63
Sofía Elena Rubio Avila <srubioavila227@gmail.com>
2022-06-07 23:07:33 -0600

    Load images

--- Menu.py
+++ Menu.py
@@ -1,18 +1,24 @@
 #Menú 2
 
+from email.mime import image
 from tkinter import *
 import random
 import time
 import Ayuda
+import salon
 
+global menu
 
 
 def main():
+    
     menu = Tk()
     menu.title("BattleShip: Segundo Proyecto Programado")
     menu.resizable(False, False) # No se puede cambiar el tamaño de la ventana
     menu.wm_attributes("-topmost", 1) #Poner esta ventana enfrente de todas las demas 
+    menu_imagen = PhotoImage(file = "imgs/fondo.png")
     canvas = Canvas(menu, width=1400, height=700, borderwidth =0, highlightthickness=0, bg = "#000000")
+    canvas.create_image(700,350,image=menu_imagen)
 
     canvas.pack()
     menu.update()
@@ -28,16 +34,25 @@ def main():
     label1 = Label(canvas, text = "Battle Ship", font = ("Haettenschweiler", 100), bg="#000000", fg="#FFFF00")
     label1.place(x=550, y=50)
 
- 
-    button_start_again = Button(canvas, text="", font=("fixedsys", "30"), bg = "#3B83BD")
+
+    iniciar_imagen = PhotoImage(file = "imgs/INICIAR.png")
+    button_start_again = Button(canvas, text="", image = iniciar_imagen, font=("fixedsys", "30"), bg="#000000")
     button_start_again.place(x=200, y=300)
 
-
-    button_salon_de_la_fama = Button(canvas, text="Salon Fama", command= lambda : Ayuda.SalonFama(menu), font=("fixedsys", "30"), bg= "#008000")
+    salon_imagen = PhotoImage(file = "imgs/SALON.png")
+    button_salon_de_la_fama = Button(canvas, text="Salon Fama", image = salon_imagen, command= lambda : salon.SalonFama(menu), font=("fixedsys", "30"), bg= "#000000")
     button_salon_de_la_fama.place(x=1000, y=300)
 
-    button_ayuda = Button(canvas, text="Ayuda", font=("fixedsys", "30"), command= lambda : Ayuda.ayuda_juego(menu) , bg= "#3B83BD")
+
+    ayuda_imagen = PhotoImage(file = "imgs/AYUDA.png")
+    button_ayuda = Button(canvas, text="Ayuda", font=("fixedsys", "30"), image = ayuda_imagen,command= lambda : Ayuda.ayuda_juego(menu) , bg= "#000000")
     button_ayuda.place(x=675, y=500)
+
+
  
     menu.mainloop()
-main()
+
+
+
+if __name__ == "__main__":
+    main()
